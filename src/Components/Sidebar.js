4771e9f990bb25f83ffd6eecd6a2fbed18cbbc8e
chandana105 <39641650+chandana105@users.noreply.github.com>
2021-05-06 00:53:55 +0530

    modified sidebar

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,6 +1,10 @@
 const Sidebar = () => {
   return (
     <div className="side-bar">
+       <div className="side-bar-box">
+           <span>FILTERS</span> 
+           <button class="btn btn-gp bg-red ">CLEAR ALL</button>
+           </div>     
       <ul>
         <li>
           <label>
