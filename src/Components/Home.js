7d97291b3251e5b845d39711448e23bfbd1286e6
chandana105 <39641650+chandana105@users.noreply.github.com>
2021-06-11 11:59:30 +0530

    minor change to home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,18 @@
 import Navbar from "./Nav";
-
+import { Link } from "react-router-dom";
 export default function Home() {
   return (
-    <div className="container">
-      <Navbar />
-    </div>
+    <>
+      <div className="container">
+        <Navbar />
+      </div>
+      <p style={{ textAlign: "center" }}>
+        Please visit{" "}
+        <Link to="/product-listing">
+          <b>Products</b>{" "}
+        </Link>{" "}
+        page
+      </p>
+    </>
   );
 }
